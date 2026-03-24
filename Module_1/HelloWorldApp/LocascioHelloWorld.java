71085ad52c768b97f2e3a41fed8c6498396f3008
Danny <dlocascio@my365.bellevue.edu>
2026-03-24 01:28:20 -0400

    Adding LocascioHelloWorld.java file.

---
+++ Module_1/HelloWorldApp/LocascioHelloWorld.java
@@ -0,0 +1,13 @@
+/*
+    Locascio, D. (2025). CIS 505 Intermediate Java Programming. Bellevue University.
+*/
+
+public class LocascioHelloWorld {
+    //Main method takes firstName and lastName String variables and outputs a "Hello World" script.
+    public static void main(String[] args) {
+        String firstName = "Danny";
+        String lastName = "Locascio";
+
+        System.out.printf ("Hello World from %s %s.", firstName, lastName);
+    }
+}
